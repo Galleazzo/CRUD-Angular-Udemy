b576b5e1bec6809d3e3da4c98ac1797c641536c7
Galleazzo <paulogalleazzo1@gmail.com>
2023-02-05 15:09:00 -0300

    criar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,28 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+
 import {MatSidenavModule} from '@angular/material/sidenav'
-import {MatListModule} from '@angular/material/list'
+import {MatCardModule} from '@angular/material/card'
+import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     MatToolbarModule,
@@ -25,7 +38,8 @@ import {MatListModule} from '@angular/material/list'
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
 
   ],
   providers: [],
